a304bc991f4cdd35e8d8f5a03ee029aacdd21040
Rahma-Obeidat <rahmaobeidat55@gmail.com>
2026-03-24 12:59:34 +0300

    upload the new

---
+++ Q2CS/Q2CS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rahma");
+            Console.WriteLine("Obeidat");
+            Console.WriteLine(1999);
+
+        }
+    }
+}
